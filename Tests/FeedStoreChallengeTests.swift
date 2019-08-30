982cdfa4d3ff407645e832b1411631f4382c00e4
Stephan Lemnitzer <lstephan.dev@gmail.com>
2019-08-30 22:23:09 +0200

    Got "test_insert_deliversNoErrorOnEmptyCache" passing.

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -39,9 +39,9 @@ final class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
-//        let sut = makeSUT()
-//
-//        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+        let sut = makeSUT()
+        
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
