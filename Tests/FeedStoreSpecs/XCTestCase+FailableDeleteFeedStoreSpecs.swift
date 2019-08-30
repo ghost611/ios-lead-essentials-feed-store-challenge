96a105d304aa41974954e0e8f4b253f40cdc0a33
Stephan Lemnitzer <lstephan.dev@gmail.com>
2019-08-30 19:08:17 +0200

    Converted tabs to spaces. :P

--- Tests/FeedStoreSpecs/XCTestCase+FailableDeleteFeedStoreSpecs.swift
+++ Tests/FeedStoreSpecs/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -6,15 +6,15 @@ import XCTest
 import FeedStoreChallenge
 
 extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
-	func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
-		let deletionError = deleteCache(from: sut)
-		
-		XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
-	}
-	
-	func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
-		deleteCache(from: sut)
-		
-		expect(sut, toRetrieve: .empty, file: file, line: line)
-	}
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+        
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+    }
+    
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        deleteCache(from: sut)
+        
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
 }
